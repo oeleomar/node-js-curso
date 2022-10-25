68ffaee93eaab14a2584650b0f92825363c4a5be
Eleomar Dorneles <eleomar.dorneles.18@outlook.com>
2022-10-25 13:37:05 -0400

    ok

--- teste-banco-prisma/src/server.ts
+++ teste-banco-prisma/src/server.ts
@@ -6,6 +6,7 @@ import { AppError } from "./errors/AppError";
 const app = express();
 app.use(express.json());
 app.use(routes);
+app.use(express.static("static"));
 app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
   if (err instanceof AppError) {
     return res.status(err.statusCode).json({
