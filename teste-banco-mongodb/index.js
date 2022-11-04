bf5be95cba53d8c23167b829c1507b1ddc53ccda
Eleomar Dorneles <eleomar.dorneles.18@outlook.com>
2022-11-04 10:11:13 -0400

    ok

--- teste-banco-mongodb/index.js
+++ teste-banco-mongodb/index.js
@@ -1,10 +1,8 @@
 import express from "express";
 import morgan from "morgan";
 import mongoose from "mongoose";
-import { router } from "./routes/index.js";
-import "dotenv";
-
-mongoose.connect("mongodb://localhost:27017/process");
+import { router } from "./src/routes/index.js";
+import * as dotenv from "dotenv";
 
 const app = express();
 app.use(express.json());
@@ -13,5 +11,4 @@ app.use(morgan("combined"));
 app.use(express.static("public"));
 
 app.use(router);
-
-app.listen(3000);
+mongoose.connect("mongodb://localhost:27017/process", () => app.listen(3000));
